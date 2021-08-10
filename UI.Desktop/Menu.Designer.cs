54b966232317309a087ad1a40a75a1b27caa38e4
Ignacio Bianchi <bianchi.ignacio97@gmail.com>
2021-08-09 21:25:09 -0300

    Creado Menu - Desktop

---
+++ UI.Desktop/Menu.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace UI.Desktop
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.alumnosNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.planesNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modulosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accesoAModulosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.profesoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materiasNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inscripcionesNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comisionesNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cursosNIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(886, 466);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(886, 490);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alumnosNIToolStripMenuItem,
+            this.usuariosNIToolStripMenuItem,
+            this.planesNIToolStripMenuItem,
+            this.modulosToolStripMenuItem,
+            this.personasToolStripMenuItem,
+            this.profesoresToolStripMenuItem,
+            this.materiasNIToolStripMenuItem,
+            this.inscripcionesNIToolStripMenuItem,
+            this.comisionesNIToolStripMenuItem,
+            this.cursosNIToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(886, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // alumnosNIToolStripMenuItem
+            // 
+            this.alumnosNIToolStripMenuItem.Name = "alumnosNIToolStripMenuItem";
+            this.alumnosNIToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.alumnosNIToolStripMenuItem.Text = "Alumnos NI";
+            // 
+            // usuariosNIToolStripMenuItem
+            // 
+            this.usuariosNIToolStripMenuItem.Name = "usuariosNIToolStripMenuItem";
+            this.usuariosNIToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.usuariosNIToolStripMenuItem.Text = "Usuarios NI";
+            // 
+            // planesNIToolStripMenuItem
+            // 
+            this.planesNIToolStripMenuItem.Name = "planesNIToolStripMenuItem";
+            this.planesNIToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.planesNIToolStripMenuItem.Text = "Planes NI";
+            // 
+            // modulosToolStripMenuItem
+            // 
+            this.modulosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.accesoAModulosToolStripMenuItem});
+            this.modulosToolStripMenuItem.Name = "modulosToolStripMenuItem";
+            this.modulosToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.modulosToolStripMenuItem.Text = "Permisos";
+            // 
+            // accesoAModulosToolStripMenuItem
+            // 
+            this.accesoAModulosToolStripMenuItem.Name = "accesoAModulosToolStripMenuItem";
+            this.accesoAModulosToolStripMenuItem.Size = new System.Drawing.Size(229, 22);
+            this.accesoAModulosToolStripMenuItem.Text = "Acceso a Modulos (Modulos)";
+            this.accesoAModulosToolStripMenuItem.Click += new System.EventHandler(this.accesoAModulosToolStripMenuItem_Click);
+            // 
+            // personasToolStripMenuItem
+            // 
+            this.personasToolStripMenuItem.Name = "personasToolStripMenuItem";
+            this.personasToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.personasToolStripMenuItem.Text = "Personas";
+            this.personasToolStripMenuItem.Click += new System.EventHandler(this.personasToolStripMenuItem_Click);
+            // 
+            // profesoresToolStripMenuItem
+            // 
+            this.profesoresToolStripMenuItem.Name = "profesoresToolStripMenuItem";
+            this.profesoresToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.profesoresToolStripMenuItem.Text = "Profesores NI";
+            // 
+            // materiasNIToolStripMenuItem
+            // 
+            this.materiasNIToolStripMenuItem.Name = "materiasNIToolStripMenuItem";
+            this.materiasNIToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.materiasNIToolStripMenuItem.Text = "Materias NI";
+            // 
+            // inscripcionesNIToolStripMenuItem
+            // 
+            this.inscripcionesNIToolStripMenuItem.Name = "inscripcionesNIToolStripMenuItem";
+            this.inscripcionesNIToolStripMenuItem.Size = new System.Drawing.Size(103, 20);
+            this.inscripcionesNIToolStripMenuItem.Text = "Inscripciones NI";
+            // 
+            // comisionesNIToolStripMenuItem
+            // 
+            this.comisionesNIToolStripMenuItem.Name = "comisionesNIToolStripMenuItem";
+            this.comisionesNIToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.comisionesNIToolStripMenuItem.Text = "Comisiones NI";
+            // 
+            // cursosNIToolStripMenuItem
+            // 
+            this.cursosNIToolStripMenuItem.Name = "cursosNIToolStripMenuItem";
+            this.cursosNIToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.cursosNIToolStripMenuItem.Text = "Cursos NI";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 490);
+            this.Controls.Add(this.toolStripContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem alumnosNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem planesNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modulosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accesoAModulosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem profesoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materiasNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inscripcionesNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comisionesNIToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cursosNIToolStripMenuItem;
+    }
+}
+
---
+++ UI.Desktop/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void accesoAModulosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Modulo mod = new Modulo();
+            mod.ShowDialog();
+        }
+
+        private void personasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Personas per = new Personas();
+            per.ShowDialog();
+        }
+    }
+}
