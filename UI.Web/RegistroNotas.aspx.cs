ad2ee827d1a11c2ebf6d5d6e272b3f1ce441e589
Ignacio Bianchi <bianchi.ignacio97@gmail.com>
2021-11-19 15:27:14 -0300

    Agregado el logic de AlumnoInscripciones.

--- UI.Web/RegistroNotas.aspx.cs
+++ UI.Web/RegistroNotas.aspx.cs
@@ -1,4 +1,5 @@
-﻿using Business.Logic;
+﻿using Business.Entities;
+using Business.Logic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,12 +13,21 @@ namespace UI.Web
     public partial class RegistroNotas : ApplicationWeb
     {
         private CursoLogic _CursoLogic = new CursoLogic();
+        private PersonaLogic _PersonaLogic = new PersonaLogic();
+        private AlumnoInscripcionLogic _AlumnoInscripcionLogic = new AlumnoInscripcionLogic();
         protected new void Page_Load(object sender, EventArgs e)
         {
             DropDownListCursos.DataSource = _CursoLogic.GetAll().Select(c => new { ID = c.ID, Descripcion = $"{c.Materia} - {c.Comision} - {c.AnioCalendario}" }).ToList();
             DropDownListCursos.DataTextField = "Descripcion";
             DropDownListCursos.DataValueField = "ID";
             DropDownListCursos.DataBind();
+        }
+
+        protected void DropDownListCursos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<Persona> alumnos = _PersonaLogic.GetAll().Where(a => a.TipoPersona == Business.Entities.Persona.TiposPersona.Alumno).ToList();
+            List<AlumnoInscripcion> inscripciones = _AlumnoInscripcionLogic.GetAll();
+            GridViewAlumnos.DataSource = 
         }
     }
 }
